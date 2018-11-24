1f46e6f769846ac862e81dadeabfd95a5c948a4a
Anthony Sepa <anthonysepa@yahoo.ca>
2018-11-24 10:43:20 -0400

    convert: dproxy-nexgen to a tarball patch format

--- bcm963xx_gpl_consumer/userspace/gpl/apps/dproxy-nexgen/dns_io.h
+++
@@ -1,5 +0,0 @@
-#include "dproxy.h"
-
-void *get_in_addr(struct sockaddr_storage *sa);
-int dns_read_packet(int sock, dns_request_t *m);
-int dns_write_packet(int sock, struct sockaddr_storage *in, dns_request_t *m);
