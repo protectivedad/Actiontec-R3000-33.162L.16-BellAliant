d6c5eb0a15471123a12a4003f9a585525d751ec6
Anthony Sepa <anthonysepa@yahoo.ca>
2018-11-24 10:47:01 -0400

    convert: bridge-utils to a tarball patch format

--- bcm963xx_gpl_consumer/userspace/gpl/apps/bridge-utils/libbridge/config.h
+++
@@ -1,9 +0,0 @@
-/* libbridge/config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated automatically from configure.in by autoheader.  */
-
-/* Name of package */
-#define PACKAGE_NAME "bridge-utils"
-
-/* Version number of package */
-#define PACKAGE_VERSION "1.2"
-
